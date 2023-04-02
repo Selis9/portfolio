9bd2bbfd4272ee166042755734c507346e321a12
Selis9 <tony.andrew.kang@gmail.com>
2023-04-02 12:47:53 -0700

    final for sure

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,15 +1,15 @@
 import { FiLinkedin, FiGithub, FiInstagram, FiMail  } from 'react-icons/fi'
 
 const actions = [
-  { icon: <FiLinkedin size="2em" className="my-8 text-neutral-500 hover:text-zinc-400"/>, name: 'Linkedin', url:'https://www.linkedin.com/in/tonyandrewkang/'},
-  { icon: <FiGithub size="2em" className="my-8 text-neutral-500 hover:text-zinc-400"/>, name: 'Github', url:'https://github.com/Selis9' },
-  { icon: <FiInstagram size="2em" className="my-8 text-neutral-500 hover:text-zinc-400"/>, name: 'Instagram', url:'https://www.instagram.com/depth.of.tonee/'  },
-  { icon: <FiMail size="2em" className="my-8 text-neutral-500 hover:text-zinc-400"/>, name: 'Email', url: 'mailto:tony.andrew.kang@gmail.com' },
+  { icon: <FiLinkedin size="2em" className="my-8 text-black hover:text-zinc-600"/>, name: 'Linkedin', url:'https://www.linkedin.com/in/tonyandrewkang/'},
+  { icon: <FiGithub size="2em" className="my-8 text-black hover:text-zinc-600"/>, name: 'Github', url:'https://github.com/Selis9' },
+  { icon: <FiInstagram size="2em" className="my-8 text-black hover:text-zinc-600"/>, name: 'Instagram', url:'https://www.instagram.com/depth.of.tonee/'  },
+  { icon: <FiMail size="2em" className="my-8 text-black hover:text-zinc-600"/>, name: 'Email', url: 'mailto:tony.andrew.kang@gmail.com' },
 ];
 
 const SocialMedia = () => {
   return (
-    <div className="fixed bottom-16 left-10 z-50 justify-center p-2"
+    <div className="fixed bottom-16 left-10 rounded-full bg-orange-200 z-50 justify-center p-2"
     >
       <div className="flex-row justify-around">
         {
